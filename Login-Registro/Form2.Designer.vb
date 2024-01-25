aa4a3fe4c38ea9aead54c78aaafa237616c77c49
leandrovitor <leandrovitorlelis@gmail.com>
2024-01-25 00:41:58 -0300

    Add files via upload

---
+++ Login-Registro/Form2.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.CadastrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoUsuárioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoProdutoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ConfiguraçõesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SobreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CadastrarToolStripMenuItem, Me.ConfiguraçõesToolStripMenuItem, Me.SobreToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(504, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'CadastrarToolStripMenuItem
+        '
+        Me.CadastrarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovoUsuárioToolStripMenuItem, Me.NovoProdutoToolStripMenuItem})
+        Me.CadastrarToolStripMenuItem.Name = "CadastrarToolStripMenuItem"
+        Me.CadastrarToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.CadastrarToolStripMenuItem.Text = "Cadastro"
+        '
+        'NovoUsuárioToolStripMenuItem
+        '
+        Me.NovoUsuárioToolStripMenuItem.Name = "NovoUsuárioToolStripMenuItem"
+        Me.NovoUsuárioToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
+        Me.NovoUsuárioToolStripMenuItem.Text = "Novo Usuário"
+        '
+        'NovoProdutoToolStripMenuItem
+        '
+        Me.NovoProdutoToolStripMenuItem.Name = "NovoProdutoToolStripMenuItem"
+        Me.NovoProdutoToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
+        Me.NovoProdutoToolStripMenuItem.Text = "Novo Produto"
+        '
+        'ConfiguraçõesToolStripMenuItem
+        '
+        Me.ConfiguraçõesToolStripMenuItem.Name = "ConfiguraçõesToolStripMenuItem"
+        Me.ConfiguraçõesToolStripMenuItem.Size = New System.Drawing.Size(96, 20)
+        Me.ConfiguraçõesToolStripMenuItem.Text = "Configurações"
+        '
+        'SobreToolStripMenuItem
+        '
+        Me.SobreToolStripMenuItem.Name = "SobreToolStripMenuItem"
+        Me.SobreToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.SobreToolStripMenuItem.Text = "Sobre"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(211, 167)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(137, 58)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "SAIR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(504, 321)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form2"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Seja Bem vindo"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents CadastrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NovoUsuárioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NovoProdutoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ConfiguraçõesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SobreToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button1 As Button
+End Class
